7732162c13ce0107e6f63025acc0308eae16a549
Max van der Schee <m.v.d.schee@ewake.nl>
2019-03-28 18:05:32 +0000

    Types documentation :memo:

--- server/src/types.ts
+++ server/src/types.ts
@@ -6,28 +6,62 @@ export interface Rule {
 	/**
 	 * The type describes what kind of rule it's 
 	 * going to implement for example `Semantic`,
-	 *  `Missing`, `Duplicant` etc.
+	 *  `Missing`, `Duplicant` etc. This describtion
+	 * whill be used as a `Diagnostic.code`.
 	 */
 	type: string;
 	/**
-	 * An identifier is an Regular expression
-     * that filters out the use full bit's of text
+	 * The identifier is an Regular expression
+     * that filters out the usefull bit's of text
      * that will be examined for any possible
      * Web Accessibility violations.
 	 * 
 	 * The first capture groep will be used.
 	 */
 	identifier: RegExp;
+	/**
+	 * The severity defines which type of error
+	 * will be shown when an "oepsie" is found.
+	 * See {@link Severity}
+	 */
 	severity: Severity;
+	/**
+	 * The messages contains the actuale warning
+	 * that explains what exactly went wrong. 
+	 */
 	message: string;
-	filters: Filter[];
+	/**
+	 * The filters contains an array of type
+	 * `Filter`. 
+	 * See {@link Filter}
+	 */
+	filters?: Filter[];
 }
-
+/**
+ * Filter holds an other indentifier
+ * that filters using the first capture group
+ * provide by the entry `intentifier`. There are
+ * multiple options that tells exactly what needs
+ * to be done with the `RegExp`.
+ * See {@link Option}
+ */
 export interface Filter {
-	indentifier: RegExp;
+	/**
+	 * The identifier within the `Filter` is 
+	 * the follow up `RegExp` that needs to be
+	 * used to find the core problem with some
+	 * Web Accessibilty rules.
+	 * 
+	 * The first capture groep will be used.
+	 */
+	identifier: RegExp;
+	/**
+	 * The options contains possible modifiers
+	 * on how to execute the `RegExp`.
+	 * See {@link Option} 
+	 */
 	options?: Option;
 }
-
 export interface Option {
 	negative?: boolean;
 	contains?: boolean;
@@ -42,3 +76,9 @@ export declare namespace Severity {
 }
 
 export declare type Severity = 1 | 2 | 3 | 4;
+
+export declare namespace Type {
+	const SEMANTIC: 'Semantic';
+	const MISSING:	'Missing';
+	const DUPLICANT: 'Duplicant';
+}
